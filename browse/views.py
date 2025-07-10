238183e764021c12a860e2a4060578c799b0314f
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-07-10 15:41:30 +0200

    detail view, get for user or skill then combine to the user with that skill

--- browse/views.py
+++ browse/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 #from skills.models import Skills
 #from user_authentication.models import UserProfile
 #from ratings.models import Rating
@@ -23,3 +23,36 @@ def browse(request):
         'current_sort': sort,
         'current_order': order
     })
+
+def skill_view(request, username=None, skill_name=None):
+    user = get_object_or_404(UserProfile, username)
+    profile = user.userprofile
+    skills = profile.skills.all()
+
+    if skill_name:
+        skill = get_object_or_404(skills, name=skill_name)
+        return render(request, 'browse/skill_detail.html', {
+            'user': user,
+            'skill': skill,
+        })
+    else:
+        return render(request, 'browse/skills_by_user.html',{
+            'user': user,
+            'skills': skills,
+        })
+    
+def reverse_skill_view(request, skill_name=None, username=None):
+    skill = get_object_or_404(Skill, name=skill_name)
+    users = UserProfile.objects.filter(userprofile_skills=skill)
+
+    if username:
+        user = get_object_or_404(users, username=username)
+        return render(request, 'browse/skill_detail.html', {
+            'user': user,
+            'skills': skill,
+        })
+    else:
+        return render(request, 'browse/user_by_skill.html', {
+            'skill': skill,
+            'users': users,
+        })
