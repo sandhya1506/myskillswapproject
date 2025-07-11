3a365ce63f2a9c31a943664b97e7713ade0d2062
sandhya1506 <sandhya.kum88@gmail.com>
2025-07-11 10:54:11 +0200

    updating

--- skillswap/urls.py
+++ skillswap/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('', include('contactapp.urls')),
+    path('admin/', admin.site.urls),
+    path('contact/', include('contactapp.urls')),
     
 ]
