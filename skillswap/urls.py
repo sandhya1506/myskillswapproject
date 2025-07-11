66f90df11b177c0314072096ed10ff333bc92a4b
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-07-11 10:43:35 +0200

    development up to date in settings and urls.

--- skillswap/urls.py
+++ skillswap/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from skills import views
-from user_authentication import views 
-from contactapp import views
-from reviewsapp import views
+# from user_authentication import views 
+# from contactapp import views
+# from reviewsapp import views
 
 
 urlpatterns = [
@@ -27,6 +27,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('skillboard/', include('skills.urls')),
     path('contact/', include('contactapp.urls')),
-    path('reviews/', include('reviewsapp.urls')),
+    # path('reviews/', include('reviewsapp.urls')),
     path('auth/', include('user_authentication.urls', namespace='user_authentication')),
 ]
