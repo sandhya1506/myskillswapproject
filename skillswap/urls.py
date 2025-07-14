f30e9ad78314195e928bc6ff5f04032acf8cfd60
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-07-14 22:03:32 +0200

    duplicate in templates, needs to update

--- skillswap/urls.py
+++ skillswap/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from skills import views
-from user_authentication import views 
 # from contactapp import views
 # from reviewsapp import views
 
