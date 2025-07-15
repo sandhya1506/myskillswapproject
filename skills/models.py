9a106b8440b233259b2ce87c5e233f39f4ba9a70
Shadigmail <shadi.shahsavani@gmail.com>
2025-07-15 09:25:25 +0200

    updated models with request

--- skills/models.py
+++ skills/models.py
@@ -9,14 +9,22 @@ CATEGORIES =  [
 ]
 
 class AddSkills(models.Model):
-    username = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     date = models.DateField()
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=2, choices=CATEGORIES, default='OT')
+    availability = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     description = models.TextField()
-    availability = models.TextField()
-    location = models.TextField()
-    profilePic = models.ImageField(default='fallback.png', blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+class RequestSkills(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    category = models.CharField(max_length=2, choices=CATEGORIES, default='OT')
+    location = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
