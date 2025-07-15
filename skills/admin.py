0f1a71aaa3b92c0b89c2732ef55e0bb983937e8e
Shadigmail <shadi.shahsavani@gmail.com>
2025-07-15 15:49:14 +0200

    updated skills/admin.py

--- skills/admin.py
+++ skills/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from skills.models import RequestSkills, AddSkills
 
 # Register your models here.
+
+admin.site.register(AddSkills)
+admin.site.register(RequestSkills)
