334e6e4981e152e5a4662547d863caefbcfcbc86
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-07-16 10:26:08 +0200

    Away with their HEADS!

--- contactapp/admin.py
+++ contactapp/admin.py
@@ -1,12 +1,6 @@
 
 from django.contrib import admin
 
-<<<<<<< HEAD
 from .models import ContactMessage
 
-=======
-
-from .models import ContactMessage
-
->>>>>>> c8a0033 (fixing admin)
 admin.site.register(ContactMessage)
