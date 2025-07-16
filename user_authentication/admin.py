4bbe3b0930f365f3b449ff05325cfd4c79c72554
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-07-16 10:04:13 +0200

    Admins up to date

--- user_authentication/admin.py
+++ user_authentication/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .models import UserProfile
 
 
@@ -9,16 +7,3 @@ from .models import UserProfile
 
 admin.site.register(UserProfile)
 
-=======
-from .models import User, UserProfile
-# from forms import UserProfileForm, Form
-
-
-# Register your models here.
-# admin.site.register(User)
-admin.site.register(UserProfile)
-# admin.site.register(UserProfileForm)
-# admin.site.register(Form)
->>>>>>> c8a0033 (fixing admin)
-=======
->>>>>>> faef519 (updating admin still)
